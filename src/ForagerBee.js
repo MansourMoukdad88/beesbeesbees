21130d0b726af53ac7198e8625662087cb229339
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2018-10-27 17:03:52 +0200

    fix beesbees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+	Bee.call(this);
+	this.age = 10;
+	this.job = "find pollen"
+	Bee.call(this.color);
+	Grub.call(this.food);
+	Grub.call(this.eat);
+	this.canFly = true;
+	this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee
+
+ForagerBee.prototype.forage = function(treasure) {
+	this.treasureChest.push(treasure);
+}
 
